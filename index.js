d561bba144c3436b98677d5903ff1adecf889bdc
dhiflaoui <dhiflaouiamira98@gmail.com>
2023-09-02 19:49:24 +0200

    add bookmodel and bookroute

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 
-import cors from 'cors';
+import { Book } from './models/bookModel.js';
+
 
 const app = express();
-
 app.use(express.json());
-
-app.use(cors());
-
 
 app.get('/', (request, response) => {
   console.log(request);
@@ -18,6 +15,62 @@ app.get('/', (request, response) => {
 
 
 
+// Route for Save a new Book
+app.post('/books', async (request, response) => {
+    try {
+      if (
+        !request.body.title ||
+        !request.body.author ||
+        !request.body.publishYear
+      ) {
+        return response.status(400).send({
+          message: 'Send all required fields: title, author, publishYear',
+        });
+      }
+      const newBook = {
+        title: request.body.title,
+        author: request.body.author,
+        publishYear: request.body.publishYear,
+      };
+  
+      const book = await Book.create(newBook);
+  
+      return response.status(201).send(book);
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
+
+// Route for Get All Books 
+app.get('/books', async (request, response) => {
+    try {
+      const books = await Book.find({});
+  
+      return response.status(200).json({
+        count: books.length,
+        data: books,
+      });
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
+
+
+  // Route for Get One Book from database by id
+router.get('/:id', async (request, response) => {
+    try {
+      const { id } = request.params;
+  
+      const book = await Book.findById(id);
+  
+      return response.status(200).json(book);
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
 mongoose
   .connect(mongoDBURL)
   .then(() => {
