95e6149e74cf3abb6a71df585139a96bbd7707c1
dhiflaoui <dhiflaouiamira98@gmail.com>
2023-09-02 18:16:08 +0200

    My first Commit

---
+++ config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://dhiflaouiamira98:OXu02p60BsWIBClX@bookstore.65qgwi5.mongodb.net/?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
